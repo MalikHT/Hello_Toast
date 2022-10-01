9ab6a59c423845ee748b9448237eacb9a5ddfe9b
MalikHT <malikhadjtaieb@outlook.fr>
2022-10-01 10:03:03 +0100

    update.

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        Button btncount = findViewById(R.id.button_count);
+
+        btncount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countUp();
+            }
+        });
     }
 
     public void showToast(View view) {
@@ -24,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
-    public void countUp(View view) {
+    public void countUp() {
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
